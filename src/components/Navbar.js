8888a94edf594a0eca6e4b642f6b431519ee2d15
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-08 15:01:03 -0800

    add additional page anchor links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,20 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-md navbar-light ml-2">
-      <a className="navbar-brand" href="#">Brand</a>
+      <a className="navbar-brand" href="#">Stephen Jeong</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarToggler">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <ul className="navbar-nav ml-auto mt-2 mr-2 mt-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" href="/#bio">Bio</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" href="#/skills">Skills</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/projects">Projects</a>
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
