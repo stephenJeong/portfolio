edeea8d0eadc489104dcf3492c052e417fce4887
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-08 14:28:20 -0800

    add max-width for better readability

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,8 @@ import './Hero.css';
 function Hero() {
   return (
     <div className="hero">
-      <h1 className="display-3">Full-Stack Software Engineer</h1>
-      <h4 className="text-muted">I enjoy using tech to leave a positive impact on our world.</h4>
+      <h1 className="display-4">Full-Stack Software Engineer</h1>
+      <h4 className="text-muted">sub heading</h4>
       <img src="https://drive.google.com/uc?id=19t56rNeBmZKienOcd-HR2sfcVIhv4Cs9" alt="Stephen Jeong" />
     </div>
   );
