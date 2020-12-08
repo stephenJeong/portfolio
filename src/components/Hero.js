42a0ae70601110342d16430d6ba8359017205b6d
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-07 22:30:14 -0800

    add hero component and style

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import './Hero.css';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="display-3">Full-Stack Software Engineer</h1>
+      <h4 className="text-muted">I enjoy using tech to leave a positive impact on our world.</h4>
+      <img src="https://drive.google.com/uc?id=19t56rNeBmZKienOcd-HR2sfcVIhv4Cs9" alt="Stephen Jeong" />
+    </div>
+  );
+}
+
+export default Hero;
