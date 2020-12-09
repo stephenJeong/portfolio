1b4e5d57605db15255b7f5039b3f8a6af5728026
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-08 22:42:35 -0800

    update hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section className="hero">
       <div>
-        <h1 className="display-4">Full-Stack Software Engineer</h1>
+        <h1 className="display-4">Hi, I'm Stephen,<br/>a full-stack web developer</h1>
         <h4>sub heading</h4>
         <img src="https://drive.google.com/uc?id=19t56rNeBmZKienOcd-HR2sfcVIhv4Cs9" alt="Stephen Jeong" />
       </div>
