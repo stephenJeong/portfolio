9dd40ca22521c8ee627f0b9815b60956134be7f4
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-15 13:16:14 -0800

    make footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,24 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <section id="footer">
-      <div className="mw-l text-center">
-        <div className="social">
-          <a className="btn" href="https://www.linkedin.com/in/stephenjeong/">
-            <i className="fa fa-linkedin fa-2x"></i>
-          </a>
+    <section id="footer" className="text-center">
+        <ul className="container">
+          <li>
+            <a className="btn" href="https://www.linkedin.com/in/stephenjeong/">
+              <i className="fa fa-linkedin fa-2x"></i>
+            </a>
+          </li>
+        <li>
           <a className="btn" href="https://github.com/stephenJeong">
             <i className="fa fa-github fa-2x"></i>
           </a>
+        </li>
+        <li>
           <a className="btn" href="mailto:stephen.h.jeong@gmail.com">
             <i className="fa fa-envelope fa-2x"></i>
           </a>
-        </div>
-      </div>
+        </li>
+        </ul>
     </section>
   );
 }
