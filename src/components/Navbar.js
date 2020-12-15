a6a6b3027dcc272b63ed29906bc66c82c428418b
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-15 13:06:40 -0800

    add background color and sticky position to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,30 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-md navbar-light ml-2">
-      <a className="navbar-brand" href="#">Stephen Jeong</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarToggler">
-        <ul className="navbar-nav ml-auto mt-2 mr-2 mt-lg-0">
-          <li className="nav-item">
-            <a className="nav-link" href="/#about">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/skills">Skills</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/projects">Projects</a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <button id="contact-bt" className="btn btn-outline-success my-2 my-sm-0" type="submit">Say Hello</button>
-        </form>
-      </div>
-    </nav>
+    <div className="navbar-container sticky-top">
+      <nav className="navbar navbar-expand-md navbar-light ml-2">
+        {/* <a className="navbar-brand" href="#">Stephen Jeong</a> */}
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className="navbar-nav ml-auto mt-2 mr-2 mt-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/skills">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/projects">Projects</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <button id="contact-bt" className="btn btn-outline-success my-2 my-sm-0" type="submit">Say Hello</button>
+          </form>
+        </div>
+      </nav>
+    </div>
   );
 }
 
