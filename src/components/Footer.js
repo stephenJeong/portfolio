2319a99a3e7560ad15df93be75e6669a57ca92b1
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-15 11:28:19 -0800

    add social icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,18 @@ import './Footer.css';
 function Footer() {
   return (
     <section id="footer">
-      <div className="mw-l">
-        Footer section
+      <div className="mw-l text-center">
+        <div>
+          <a className="btn" href="https://www.linkedin.com/in/stephenjeong/">
+            <i className="fa fa-linkedin fa-2x"></i>
+          </a>
+          <a className="btn" href="https://github.com/stephenJeong">
+            <i className="fa fa-github fa-2x"></i>
+          </a>
+          <a className="btn" href="mailto:stephen.h.jeong@gmail.com">
+            <i className="fa fa-envelope fa-2x"></i>
+          </a>
+        </div>
       </div>
     </section>
   );
