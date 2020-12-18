8c21a1e508d9c38f8e197553bdc6de214ee6b770
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-17 16:00:51 -0800

    add name to each inpute of form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,16 @@ function Form() {
       <input type="hidden" name="form-name" value="contactForm" />
       <div className="form-group">
         <label for="name">Full Name</label>
-        <input type="text" className="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Name" />
+        <input type="text" className="form-control" name="name" id="name" aria-describedby="emailHelp" placeholder="Enter Name" />
       </div>
       <div className="form-group">
         <label for="email">Email</label>
-        <input type="email" className="form-control" id="email" placeholder="Enter Email" />
+        <input type="email" className="form-control" name="email" id="email" placeholder="Enter Email" />
       </div>
-      {/* <div className="form-group">
+      <div className="form-group">
         <label for="message">Message</label><br/>
-        <textarea className="form-control" id="message" rows="4"></textarea>
-      </div> */}
+        <textarea className="form-control" name="message" id="message" rows="4"></textarea>
+      </div>
       <button type="submit" className="btn btn-primary">Submit</button>
   </form>
   );
