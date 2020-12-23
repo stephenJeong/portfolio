376d03273ee194f4d1df5594fbdd54dbf0c0e78b
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-22 19:39:39 -0800

    remove animation from hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,12 @@ import headshot from '../headshot.jpeg';
 function Hero() {
   return (
     <section className="hero text-center">
-      <div className="typewriter text-center">
-        <h1 className="display-4">Hi, I'm Stephen,</h1>
-        <h1 className="display-4">a Full-Stack Web Developer</h1>
+      <div className="text-center">
+        <h1 className="display-5">Hi, I'm Stephen,</h1>
+        <h1 className="display-5">a Full-Stack Web Developer</h1>
         {/* <h4>sub heading</h4> */}
       </div>
-      <div className="fade-in">
+      <div>
         <img src={headshot} alt="Stephen Jeong" />
       </div>
     </section>
