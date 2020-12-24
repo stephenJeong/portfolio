d16ff8abd1f7964de9acb2f18ca09f270a61ef59
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-23 19:00:48 -0800

    update to thank you class

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -3,7 +3,7 @@ function ThankYou() {
   return (
     <section className="text-center mw-l" id="thankYou">
       <div className="text-center">
-        <h1 className="display-5">Thanks for reaching out! I'll try to get back to you before you finish your coffee but just in case I don't, here's Patton Oswalt invoking the citizen filibuster on Parks and Recreation to keep you company.</h1>
+        <h1 className="display-5 thank-you">Thanks for reaching out! I'll try to get back to you before you finish your coffee but just in case I don't, here's Patton Oswalt invoking the citizen filibuster on Parks and Recreation to keep you company.</h1>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/5BBhNkywMJY?start=42" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </section>
