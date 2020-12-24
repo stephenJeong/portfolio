0767fcb2cd4296896b6dd953309a506173b3e28f
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-23 18:20:39 -0800

    change netlify form back to what it was

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,8 @@ class Form extends Component {
   }
   render() {
     return (
-      <form className="text-left" name="contactForm" method="post" data-netlify="true" id="contactForm2" action="/thank-you#top" onSubmit={this.submitForm.bind(this)}>
+      // <form className="text-left" name="contactForm" method="post" data-netlify="true" id="contactForm2" action="/thank-you#top" onSubmit={this.submitForm.bind(this)}>
+      <form className="text-left" name="contactForm" method="post">
         <input type="hidden" name="form-name" value="contactForm" />
         <div className="form-group">
           <label for="name">Full Name</label>
