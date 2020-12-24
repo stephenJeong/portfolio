44b32b1ce16bfabcc088c7c7812afb558dd3714f
Stephen Jeong <stephen.h.jeong@gmail.com>
2020-12-23 16:41:25 -0800

    add extra code to handle thank you page routing

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,32 @@
-function Form() {
-  return (
-    <form className="text-left" name="contactForm" method="post">
-      <input type="hidden" name="form-name" value="contactForm" />
-      <div className="form-group">
-        <label for="name">Full Name</label>
-        <input type="text" className="form-control" name="name" id="name" aria-describedby="emailHelp" placeholder="Enter Name" />
-      </div>
-      <div className="form-group">
-        <label for="email">Email</label>
-        <input type="email" className="form-control" name="email" id="email" placeholder="Enter Email" />
-      </div>
-      <div className="form-group">
-        <label for="message">Message</label><br/>
-        <textarea className="form-control" name="message" id="message" rows="4"></textarea>
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-  </form>
-  );
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Form extends Component {
+  submitForm(e) {
+     e.preventDefault();
+     this.props.history.push('/thank-you');
+  }
+  render() {
+
+    return (
+      <form className="text-left" name="contactForm" method="post">
+        <input type="hidden" name="form-name" value="contactForm" />
+        <div className="form-group">
+          <label for="name">Full Name</label>
+          <input type="text" className="form-control" name="name" id="name" aria-describedby="emailHelp" placeholder="Enter Name" />
+        </div>
+        <div className="form-group">
+          <label for="email">Email</label>
+          <input type="email" className="form-control" name="email" id="email" placeholder="Enter Email" />
+        </div>
+        <div className="form-group">
+          <label for="message">Message</label><br/>
+          <textarea className="form-control" name="message" id="message" rows="4"></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    );
+  }
 }
 
-export default Form;
+export default withRouter(Form);
---
+++ src/components/ThankYou.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function ThankYou() {
+  return (
+    <div className="App" id="thank-you">
+      <Navbar />
+      <div> thank you page</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default ThankYou;
